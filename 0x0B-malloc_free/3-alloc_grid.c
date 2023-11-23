1af17f7eee4c13b3404c3c521773eb984ff7ce08
Jamiil007 <oussama.jamil.11@.com>
2023-11-23 05:56:49 -0800

    new commits

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* alloc_grid - function that allocates memory for a 2D grid of integers
+* @width: width of the grid
+* @height: height of the grid
+* Return: pointer to the newly allocated grid or
+* NULL if width or height is <= 0
+* Author: NourMellal
+*/
+
+int **alloc_grid(int width, int height)
+{
+int **twod;
+int hgt_index, wid_index;
+
+if (width <= 0 || height <= 0)
+	return (NULL);
+
+twod = malloc(sizeof(int *) * height);
+
+if (twod == NULL)
+	return (NULL);
+
+for (hgt_index = 0; hgt_index < height; hgt_index++)
+{
+	twod[hgt_index] = malloc(sizeof(int) * width);
+
+	if (twod[hgt_index] == NULL)
+	{
+		for (; hgt_index >= 0; hgt_index--)
+			free(twod[hgt_index]);
+
+		free(twod);
+		return (NULL);
+	}
+}
+
+for (hgt_index = 0; hgt_index < height; hgt_index++)
+{
+	for (wid_index = 0; wid_index < width; wid_index++)
+		twod[hgt_index][wid_index] = 0;
+}
+return (twod);
+
+}
